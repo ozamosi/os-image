bdde546ea31ad4a7d70ee65a11674f0b199a65c4
Robin Sonefors <robin@spill.chat>
2023-08-02 17:55:40 +0100

    real docker plz

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\
 curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${RELEASE}"/ublue-os-staging-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/_copr_kylegospo_oversteer.repo https://copr.fedorainfracloud.org/coprs/kylegospo/oversteer/repo/fedora-"${RELEASE}"/kylegospo-oversteer-fedora-"${RELEASE}".repo
 
+RUN curl -L https://download.docker.com/linux/fedora/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+
 rpm-ostree install \
     /tmp/rpms/*.rpm \
     /tmp/akmods-rpms/*.rpm \
