5d6f9bbb7b97ae6161066f2f26d0d9a40be5707d
Robin Sonefors <robin@spill.chat>
2023-08-02 17:55:40 +0100

    real docker plz

--- install.sh
+++ install.sh
@@ -19,6 +19,8 @@ curl -Lo /etc/yum.repos.d/_copr_ublue-os_packages.repo https://copr.fedorainfrac
 curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${RELEASE}"/ublue-os-staging-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/_copr_kylegospo_oversteer.repo https://copr.fedorainfracloud.org/coprs/kylegospo/oversteer/repo/fedora-"${RELEASE}"/kylegospo-oversteer-fedora-"${RELEASE}".repo
 
+RUN curl -L https://download.docker.com/linux/fedora/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+
 rpm-ostree install \
     ublue-os-just \
     ublue-os-luks \
