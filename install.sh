6c5fddb713366bcfcd16a8fde435a828a63c0e3e
Robin Sonefors <robin@sonefors.net>
2024-07-28 17:35:16 +0100

    tailscale

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ curl -Lo /etc/yum.repos.d/_copr_kylegospo_oversteer.repo https://copr.fedorainfr
 
 RUN curl -L https://download.docker.com/linux/fedora/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
 
+curl -L https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
+
 rpm-ostree install \
     /tmp/rpms/*.rpm \
     /tmp/akmods-rpms/*.rpm \
